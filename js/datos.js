06a2101bd4b7a8faeaf710b1ef39cadbdbfe570e
javimammana <javiermammana.dev@gmail.com>
2025-10-03 11:28:51 -0300

    BCD

---
+++ js/datos.js
@@ -0,0 +1,11 @@
+
+const fecha = new Date("11/01/2025 16:00");
+
+const horaEvento = "el 01/11/2026 a las 16:00Hs";
+const direccionLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.1284512865923!2d-64.16018682507084!3d-31.43813049730166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a2cfdafe42cd%3A0x9f047a13514235fd!2sAsturias%201935%2C%20X5014BQE%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1759469311270!5m2!1ses-419!2sar";
+const direccionLink2 = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.223650489656!2d-64.153563525071!3d-31.43550929717488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a2cda6d69c43%3A0x248939c05f1ac895!2sAsturias%202712%2C%20X5014BQV%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1759470748595!5m2!1ses-419!2sar";
+const coordenadas = "Asturias 1935";
+const coordenadas2 = "Asturias 2712";
+const formAsistencia = "https://wa.me/5493512359505";
+const formCanciones = "https://docs.google.com/forms/d/e/1FAIpQLSf0bIShEgqtJWvriLv3M4YBJZEPxfaJva1gAY7NqBL5cEB_8g/viewform?usp=header";
+const fechaGoogle = "https://calendar.app.google/iJuSvoi9M3pCmRTw9";
